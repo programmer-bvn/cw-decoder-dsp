86506bf91dc9193f5395ce6664b9bd297812acc4
Mariusz Lisowski (SQ2BVN) <229903418+programmer-bvn@users.noreply.github.com>
2026-09-09 09:43:30 +0200

    bvn.bat + bvn_z.bat: skroty do kopiowania w obie strony
    Zostaja w KORZENIU, nie w srodowisko/ -- przeniesienie do podkatalogu
    dodaloby krok nawigacji do czegos, czego caly sens polega na jego braku.
    Powstaly, zeby oszczedzic operacji przy pracy w przerwach: nie ustalac
    i nie wklepywac sciezki do HDD.
    
    Dolozony kierunek powrotny. na_hdd.bat i z_hdd.bat przyjmuja sciezke
    jako pierwszy argument, wiec drugi skrot kosztowal dwie linie, a
    oszczedza to samo na koncu sesji, co bvn.bat na poczatku.
    
    Sciezka wpisana na sztywno jest tu CELEM, nie zaniedbaniem.

---
+++ bvn.bat
@@ -0,0 +1,2 @@
+na_hdd.bat c:\Users\ADMIN\PyCharmMiscProject\FT1-TF2-convert\CLAU
+pause
---
+++ bvn_z.bat
@@ -0,0 +1,5 @@
+rem Powrot: WYNIKI z HDD na pendraka, po treningu. Odwrotnosc bvn.bat.
+rem Sciezka wpisana na sztywno SWIADOMIE -- caly sens tego pliku polega
+rem na tym, zeby jej nie ustalac i nie wklepywac.
+z_hdd.bat c:\Users\ADMIN\PyCharmMiscProject\FT1-TF2-convert\CLAU
+pause
